bde729525488f561942ecd72f278253cb73cba37
TuanCho670 <tuanbmtx00019@gmail.com>
2024-12-04 16:06:49 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,16 +1,14 @@
 import os
 
-def open_app(bundle_id):
-    """Mở ứng dụng qua bundle ID"""
-    command = f"open {bundle_id}"
+def open_app_via_activator(bundle_id):
+    """Mở ứng dụng thông qua Activator"""
+    command = f"activator send {bundle_id}"
     result = os.system(command)
     if result == 0:
-        print(f"Đã mở ứng dụng với Bundle ID: {bundle_id}")
+        print(f"Đã mở ứng dụng với Activator: {bundle_id}")
     else:
-        print(f"Lỗi khi mở ứng dụng với Bundle ID: {bundle_id}")
+        print(f"Lỗi khi mở ứng dụng với Activator: {bundle_id}")
 
-# Gọi hàm với Bundle ID của ứng dụng
-bundle_id = "com.apple.mobilesafari"  # Thay bằng Bundle ID bạn muốn
-open_app(bundle_id)
-
-# apt install com.rpetrich.rocketbootstrap
+# Thay Bundle ID của ứng dụng
+bundle_id = "com.apple.mobilesafari"
+open_app_via_activator(bundle_id)
